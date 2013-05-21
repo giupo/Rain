14c1ecbc23b76f00f430a3592aae1c32aef14463
Giuseppe Acito <giuseppe.acito@gmail.com>
2013-05-21 10:47:18 +0200

    Initial Commit

---
+++ Rain/GAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GAAppDelegate.h
+//  Rain
+//
+//  Created by Giuseppe Acito on 21/05/13.
+//  Copyright (c) 2013 GA Software Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Rain/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rain
+//
+//  Created by Giuseppe Acito on 21/05/13.
+//  Copyright (c) 2013 GA Software Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GAAppDelegate class]));
+    }
+}
